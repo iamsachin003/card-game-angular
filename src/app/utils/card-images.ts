d9d1675b564abd4372ad911b9613b6f313bc48b7
iamsachin003 <sachin.makhija1@gmail.com>
2020-11-22 02:37:10 +0530

    added fix for images URL

--- src/app/utils/card-images.ts
+++ src/app/utils/card-images.ts
@@ -1,12 +1,12 @@
 export const CARD_IMAGES = [
-    '/assets/2.jpg',
-    '/assets/2.jpg',
-    '/assets/3.jpg',
-    '/assets/3.jpg',
-    '/assets/a.jpg',
-    '/assets/a.jpg',
-    '/assets/joker.jpg',
-    '/assets/joker.jpg',
-    '/assets/king.jpg',
-    '/assets/king.jpg'
+    'assets/2.jpg',
+    'assets/2.jpg',
+    'assets/3.jpg',
+    'assets/3.jpg',
+    'assets/a.jpg',
+    'assets/a.jpg',
+    'assets/joker.jpg',
+    'assets/joker.jpg',
+    'assets/king.jpg',
+    'assets/king.jpg'
 ]
